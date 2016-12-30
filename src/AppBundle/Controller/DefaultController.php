33ea1a62a305265dcc09f09663938daf013e2b07
Sylvain Metayer <sylvain_metayer@outlook.fr>
2016-12-30 16:38:25 +0100

    Fix Question 4 trailing slash from homepage

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -77,5 +78,19 @@ class DefaultController extends Controller
     public function loginAction()
     {
         return [];
+    }
+
+    /**
+     * @Route("/{url}", name="remove_trailing_slash",
+     *     requirements={"url" = ".*\/$"}, methods={"GET"})
+     */
+    public function removeTrailingSlashAction(Request $request)
+    {
+        $pathInfo = $request->getPathInfo();
+        $requestUri = $request->getRequestUri();
+
+        $url = str_replace($pathInfo, rtrim($pathInfo, ' /'), $requestUri);
+
+        return $this->redirect($url, 301);
     }
 }
